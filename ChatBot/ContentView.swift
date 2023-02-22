d41f830b7ed979e850c7516df01163fa238bb078
dwaynediddy <burrows.dwayne@icloud.com>
2023-02-22 15:56:31 +0100

    INITIAL PUSH

--- ChatBot/ContentView.swift
+++ ChatBot/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        LazyVStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
